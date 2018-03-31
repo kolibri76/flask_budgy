0f9b98275a3ef382b23752dfeb3c9e3cf66a0434
Martin <mem@kohlerma.ch>
2018-03-31 16:11:32 +0200

    ...
    Signed-off-by: Martin <mem@kohlerma.ch>

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    log = db.relationship('Ulog', backref='user', lazy='dynamic')
+    ulog = db.relationship('Ulog', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<user {}>'.format(self.email)
@@ -15,15 +15,18 @@ class User(db.Model):
 class Ulog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, default=datetime.utcnow)
-    action_id = db.Column(db.Integer, db.ForeignKey('action.id'))
+    action_id = db.Column(db.Integer, db.ForeignKey('uaction.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    uaction_id = db.Column(db.Integer, db.ForeignKey('uaction.id'))
 
 
 class Uaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), index=False, unique=True)
-    description = db.Column(db.String(120),'uaction', lazy='dynamic')
+    description = db.Column(db.String(120))
+    ulog = db.relationship('Ulog', backref='uaction', lazy='dynamic')
+
+    def __repr__(self):
+        return '<action {}>'.format(self.name)
 
 
 
